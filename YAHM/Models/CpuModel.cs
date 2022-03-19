a2408b4ed911389932ee256a9f5b293528c9f652
Tlaster <tlaster@outlook.com>
2022-03-20 04:10:02 +0800

    complete cpu sensor

--- YAHM/Models/CpuModel.cs
+++ YAHM/Models/CpuModel.cs
@@ -8,18 +8,25 @@ namespace Tlaster.YAHM.Models;
 public record CpuModel(
     string Name,
     string Vendor,
-    string Architecture,
-    string Speed,
-    string Cores,
-    string Threads,
-    string Temperature)
+    float Speed,
+    int Cores,
+    int Threads,
+    float Power,
+    float Temperature,
+    float Load)
 {
-    public static readonly CpuModel Empty = new CpuModel("", "", "", "", "", "", "");
+    public static readonly CpuModel Empty = new ("", "", 0f, 0, 0, 0, 0, 0);
 
     public static CpuModel FromGenericCpu(GenericCpu cpu)
     {
-        var temp = cpu.Sensors.Where(s => s.SensorType == SensorType.Temperature);
-        var temperature = cpu.Sensors.FirstOrDefault(s => s.SensorType == SensorType.Temperature)?.Value?.ToString() ?? string.Empty;
-        return new CpuModel(cpu.Name, "", "", "", "", "", temperature);
+        var name = cpu.CpuId.FirstOrDefault()?.FirstOrDefault()?.BrandString ?? cpu.Name ?? string.Empty;
+        var temperature = cpu.Sensors.FirstOrDefault(s => s.SensorType == SensorType.Temperature)?.Value ?? 0f;
+        var vendor = cpu.CpuId.FirstOrDefault()?.FirstOrDefault()?.Vendor.ToString() ?? string.Empty;
+        var core = cpu.Sensors.Count(it => it.SensorType == SensorType.Factor);
+        var thread = cpu.CpuId.Length;
+        var power = cpu.Sensors.FirstOrDefault(s => s.SensorType == SensorType.Power)?.Value ?? 0f;
+        var speed = cpu.Sensors.FirstOrDefault(s => s.SensorType == SensorType.Clock)?.Value ?? 0f;
+        var load = cpu.Sensors.ElementAtOrDefault(cpu.Sensors.Count(s => s.SensorType == SensorType.Load) - 1)?.Value ?? 0f;
+        return new CpuModel(name, vendor, speed, core, thread, power, temperature, load);
     }
 }
